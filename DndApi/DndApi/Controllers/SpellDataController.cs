eec525fc70a2dc9cdf0e05cc44a316388886da8b
EpsteinRoss-FS <repstein@student.fullsail.edu>
2024-04-20 11:59:29 -0400

    Gets spell data

---
+++ DndApi/DndApi/Controllers/SpellDataController.cs
@@ -0,0 +1,36 @@
+﻿using DndApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+using static DndApi.Models.SpellData;
+
+namespace DndApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpellDataController(HttpClient client) : ControllerBase
+    {
+        private HttpClient _client = client;
+        private string baseUrl = "https://www.dnd5eapi.co/api/spells/";
+
+        [HttpGet]
+        public async Task<object> GetSpellData(string id)
+        {
+
+            var response = await _client.GetAsync(baseUrl + id);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return NotFound();
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var spellData = JsonConvert.DeserializeObject<Spell>(content);
+
+            return Ok(spellData);
+
+        }
+
+    }
+}
---
+++ DndApi/DndApi/Models/SpellData.cs
@@ -0,0 +1,77 @@
+﻿namespace DndApi.Models
+{
+    public class SpellData
+    {
+
+
+
+    public class DamageType
+    {
+        public string? index { get; set; }
+        public string? name { get; set; }
+        public string? url { get; set; }
+    }
+
+    public class DamageAtSlotLevel
+    {
+        public string? _2 { get; set; }
+        public string? _3 { get; set; }
+        public string? _4 { get; set; }
+        public string? _5 { get; set; }
+        public string? _6 { get; set; }
+        public string? _7 { get; set; }
+        public string? _8 { get; set; }
+        public string? _9 { get; set; }
+    }
+
+    public class Damage
+    {
+        public DamageType? damage_type { get; set; }
+        public DamageAtSlotLevel? damage_at_slot_level { get; set; }
+    }
+
+    public class School
+    {
+        public string? index { get; set; }
+        public string? name { get; set; }
+        public string? url { get; set; }
+    }
+
+    public class Class
+    {
+        public string? index { get; set; }
+        public string? name { get; set; }
+        public string? url { get; set; }
+    }
+
+    public class Subclass
+    {
+        public string? index { get; set; }
+        public string? name { get; set; }
+        public string? url { get; set; }
+    }
+
+    public class Spell
+    {
+        public string? index { get; set; }
+        public string? name { get; set; }
+        public List<string>? desc { get; set; }
+        public List<string>? higher_level { get; set; }
+        public string? range { get; set; }
+        public List<string>? components { get; set; }
+        public string? material { get; set; }
+        public bool? ritual { get; set; }
+        public string? duration { get; set; }
+        public bool? concentration { get; set; }
+        public string? casting_time { get; set; }
+        public int? level { get; set; }
+        public string? attack_type { get; set; }
+        public Damage? damage { get; set; }
+        public School? school { get; set; }
+        public List<Class>? classes { get; set; }
+        public List<Subclass>? subclasses { get; set; }
+        public string? url { get; set; }
+    }
+
+    }
+}

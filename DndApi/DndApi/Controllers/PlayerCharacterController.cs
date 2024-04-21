0416ccbc346b34c6b9f9f40bca1e41455cd041e9
EpsteinRoss-FS <repstein@student.fullsail.edu>
2024-04-21 11:17:50 -0400

    Gets a list of ability scores

---
+++ DndApi/DndApi/Controllers/PlayerCharacterController.cs
@@ -0,0 +1,40 @@
+﻿using DndApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DndApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerCharacterController(HttpClient client) : ControllerBase
+    {
+        private HttpClient _client = client;
+        private string baseUrl = "https://www.dnd5eapi.co/api";
+
+        [HttpGet]
+        [Route("/getAllAbilityScores")]
+        public async Task<ActionResult<PlayerCharacterDataModel.PlayerAbilityScores.AbilityScoreList>> GetAllAbilityScores()
+        {
+            try
+            {
+                // URL from where you fetch the data
+
+
+                // Fetch data from the URL
+                var abilityScoreList = await _client.GetFromJsonAsync<PlayerCharacterDataModel.PlayerAbilityScores.AbilityScoreList>($"{ baseUrl}/ability-scores");
+
+                if (abilityScoreList == null)
+                {
+                    return NotFound();
+                }
+
+                return abilityScoreList;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ DndApi/DndApi/Models/PlayerCharacterDataModel.cs
@@ -0,0 +1,25 @@
+﻿namespace DndApi.Models
+{
+    public class PlayerCharacterDataModel
+    {
+
+        public class PlayerAbilityScores
+        {
+            public class AbilityScoreList
+            {
+                public int? Count { get; set; }
+                public List<AbilityScoreResults>? Results { get; set; }
+            }
+
+            public class AbilityScoreResults
+            {
+                public string? Index { get; set; }
+                public string? Name { get; set; }
+                public string? Url { get; set; }
+            }
+        }
+
+    }
+
+    
+}

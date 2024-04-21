d4ca3fd4b7c111d5b9ba3ba2ac0b7ebbaff841aa
EpsteinRoss-FS <repstein@student.fullsail.edu>
2024-04-20 23:42:04 -0400

    Gets all races

---
+++ DndApi/DndApi/Controllers/RacesController.cs
@@ -0,0 +1,40 @@
+﻿using DndApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DndApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RacesController(HttpClient client) : ControllerBase
+    {
+        private HttpClient _client = client;
+        private string baseUrl = "https://www.dnd5eapi.co/api/races/";
+
+
+
+        [HttpGet]
+        [Route("/getAllRaces")]
+        public async Task<ActionResult<RacesList>> GetAllRaces()
+        {
+            try 
+            {
+                var _racesList = await _client.GetFromJsonAsync<RacesList>(baseUrl);
+
+                if(_racesList == null) 
+                {
+                    return NotFound();
+                        
+                }
+                return _racesList;
+            }
+
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+
+        }
+
+    }
+}
---
+++ DndApi/DndApi/Models/RacesData.cs
@@ -0,0 +1,20 @@
+﻿namespace DndApi.Models
+{
+    public class RacesData
+    {
+      
+    }
+
+    public class RacesList
+    {
+        public int? Count { get; set; }
+        public List<Races>? Results { get; set; }
+    }
+
+    public class Races
+    {
+        public string? Index { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+    }
+}

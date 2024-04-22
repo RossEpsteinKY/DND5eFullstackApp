ef4b0e13f429e8f55c490151e591152144f0bdf2
EpsteinRoss-FS <repstein@student.fullsail.edu>
2024-04-22 12:27:58 -0400

    Gets damage types data

--- DndApi/DndApi/Controllers/RulesController.cs
+++ DndApi/DndApi/Controllers/RulesController.cs
@@ -43,5 +43,37 @@ namespace DndApi.Controllers
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
+
+        [HttpGet]
+        [Route("/getDamageType/{id}")]
+        public async Task<object> GetDamageType(string id)
+        {
+            try
+            {
+                // URL from where you fetch the data
+
+
+                // Fetch data from the URL
+                var response = await _client.GetAsync($"{baseUrl}/damage-types/{id}");
+
+                if (response == null)
+                {
+                    return NotFound();
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                var damageTypesData = JsonConvert.DeserializeObject<RulesModel.DamageTypes.DamageTypeData>(content);
+
+
+
+                return Ok(damageTypesData);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
     }
 }

9a2ac9a425e1c3793a1cf87121dc7bec20938b08
EpsteinRoss-FS <repstein@student.fullsail.edu>
2024-04-22 11:35:37 -0400

    Working on dynamically getting player character features by level.  Will return to this portion later.

--- DndApi/DndApi/Controllers/PlayerCharacterController.cs
+++ DndApi/DndApi/Controllers/PlayerCharacterController.cs
@@ -1,7 +1,10 @@
 ﻿using DndApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Dynamic;
+
 using static DndApi.Models.PlayerCharacterDataModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DndApi.Controllers
 {
@@ -11,214 +14,29 @@ namespace DndApi.Controllers
     public class PlayerCharacterController(HttpClient client) : ControllerBase
     {
         private HttpClient _client = client;
-        private string baseUrl = "https://www.dnd5eapi.co/api";
-        
-        
-        /**
-            [HttpGet]
-            [Route("/getAllAbilityScores")]
-            public async Task<ActionResult<CharacterDataModel.AbilityScores.AbilityScoreList>> GetAllAbilityScores()
+        private string baseUrl = "https://www.dnd5eapi.co/api/";
+
+        [HttpGet]
+        [Route("/testingTheReturn/{id}")]
+        public async Task<object> GetClassData(string id)
+        {
+
+            var response = await _client.GetAsync($"{baseUrl}/classes/{id}");
+
+            if (!response.IsSuccessStatusCode)
             {
-                try
-                {
-                    // URL from where you fetch the data
-
-
-                    // Fetch data from the URL
-                    var abilityScoreList = await _client.GetFromJsonAsync<CharacterDataModel.AbilityScores.AbilityScoreList>($"{baseUrl}/ability-scores");
-
-                    if (abilityScoreList == null)
-                    {
-                        return NotFound();
-                    }
-
-                    return abilityScoreList;
-                }
-                catch (Exception ex)
-                {
-                    // Log the exception or handle it appropriately
-                    return StatusCode(500, $"Internal server error: {ex.Message}");
-                }
+                return NotFound();
             }
 
-            [HttpGet]
-            [Route("/getAbilityScoreDetails/{id}")]
-            public async Task<object> getAbilityScoreDetails(string id)
-            {
-                try
-                {
-                    var response = await _client.GetAsync($"{baseUrl}/ability-scores/{id}");
+            var content = await response.Content.ReadAsStringAsync();
 
-                    if (response == null)
-                    { return NotFound();
-                    }
+            var classData = JsonConvert.DeserializeObject<object>(content);
 
-                    //var content = await response.Content.ReadAsStringAsync();
-                    //var monsterData = JsonConvert.DeserializeObject<MonsterData>(content);
+            var formattedData = JsonConvert.DeserializeObject<dynamic>(content)!;
 
-                    var content = await response.Content.ReadAsStringAsync();
-                    var abilityScoreDetails = JsonConvert.DeserializeObject<CharacterDataModel.AbilityScoreDetails>(content);
+            return Ok(formattedData);
 
+        }
 
-                    return Ok(abilityScoreDetails);
-
-                }
-                catch (Exception ex)
-                {
-                    return NotFound(ex);
-                }
-            }
-
-            [HttpGet]
-            [Route("/getAlignments/")]
-            public async Task<ActionResult<CharacterDataModel.Alignments.AlignmentsList>> GetListOfAlignments()
-            {
-                try
-                {
-                    var alignmentsList = await _client.GetFromJsonAsync<CharacterDataModel.Alignments.AlignmentsList>($"{baseUrl}/alignments");
-
-                    if (alignmentsList == null)
-                    {
-                        return NotFound();
-                    }
-
-                    return alignmentsList;
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex);
-                }
-
-            }
-
-            [HttpGet]
-            [Route("/getAlignmentDetails/{id}/")]
-            public async Task<object> GetAlignmentDetails(string id)
-            {
-                try
-                {
-                    var response = await _client.GetAsync($"{baseUrl}/alignments/{id}");
-
-                    if (response == null)
-                    {
-                        return NotFound();
-                    }
-
-                    //var content = await response.Content.ReadAsStringAsync();
-                    //var monsterData = JsonConvert.DeserializeObject<MonsterData>(content);
-
-                    var content = await response.Content.ReadAsStringAsync();
-                    var alignmentDetails = JsonConvert.DeserializeObject<CharacterDataModel.Alignments.AlignmentsDetails>(content);
-
-
-                    return Ok(alignmentDetails);
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex);
-                }
-
-            }
-
-            [HttpGet]
-            [Route("/getLanguages/")]
-            public async Task<ActionResult<CharacterDataModel.Languages.LanguagesList>> GetListOfLanguages()
-            {
-                try
-                {
-                    var languagesList = await _client.GetFromJsonAsync<CharacterDataModel.Languages.LanguagesList>($"{baseUrl}/languages");
-
-                    if (languagesList == null)
-                    {
-                        return NotFound();
-                    }
-
-                    return languagesList;
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex);
-                }
-
-            }
-
-            [HttpGet]
-            [Route("/getLanguageDetails/{id}")]
-            public async Task<object> GetLanguageDetails(string id)
-            {
-                try
-                {
-
-                    var response = await _client.GetAsync($"{baseUrl}/languages/{id}");
-
-                    if (response == null)
-                    {
-                        return NotFound();
-                    }
-
-                    var content = await response.Content.ReadAsStringAsync();
-                    var languageDetails = JsonConvert.DeserializeObject<CharacterDataModel.Languages.LanguagesDetails>(content);
-
-                    return Ok(languageDetails);
-
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex);
-                }
-            }
-
-            [HttpGet]
-            [Route("/getProficiencies")]
-            public async Task<ActionResult<CharacterDataModel.Proficiencies.ProficienciesList>> GetProficienciesList()
-            {
-                try
-                {
-
-                    var proficienciesList = await _client.GetFromJsonAsync<CharacterDataModel.Proficiencies.ProficienciesList>($"{baseUrl}/proficiencies");
-                    if (proficienciesList == null)
-                    {
-                        return NotFound();
-                    }
-
-                    return proficienciesList;
-
-                } 
-                catch (Exception ex) 
-                { 
-                    return BadRequest(ex); 
-                }
-            }
-
-            [HttpGet]
-            [Route("/getProficiency/{id}")]
-            public async Task<object> GetProficiency(string id)
-            {
-                try { 
-
-            
-                    var response = await _client.GetAsync($"{baseUrl}/proficiencies/{id}");
-
-                    if (response == null)
-                    {
-                        return NotFound();
-                    }
-
-                    var content = await response.Content.ReadAsStringAsync();
-                    
-                    
-                    var proficiencyDetails = JsonConvert.DeserializeObject<CharacterDataModel.Proficiencies.ProficiencyDetails>(content);
-                    
-
-                    return Ok(proficiencyDetails);
-
-                }
-                catch (Exception ex)
-                {
-                    return BadRequest(ex);
-                }
-               
-            }
-        }**/
     }
 }
--- DndApi/DndApi/Models/PlayerCharacterDataModel.cs
+++ DndApi/DndApi/Models/PlayerCharacterDataModel.cs
@@ -1,13 +1,75 @@
-﻿using static DndApi.Models.ReusableModels;
+﻿using static DndApi.Models.ClassesData;
+using static DndApi.Models.ReusableModels;
 
 namespace DndApi.Models
 {
     public class PlayerCharacterDataModel
     {
 
-        
+        /**
+                public class PlayerResources
+                {
+                    public class ClassResourcesByLevel
+                    {
+                        public class Spellcasting
+                        {
+                            public int? Cantrips_Known { get; set; }
+                            public int? Spells_Known { get; set; }
+                            public int? Spell_Slots_Level_1 { get; set; }
+                            public int? Spell_SlotsLevel_2 { get; set; }
+                            public int? Spell_Slots_Level_3 { get; set; }
+                            public int? Spell_Slots_Level_4 { get; set; }
+                            public int? Spell_Slots_Level_5 { get; set; }
+                            public int? Spell_Slots_Level_6 { get; set; }
+                            public int? Spell_Slots_Level_7 { get; set; }
+                            public int? Spell_Slots_Level_8 { get; set; }
+                            public int? Spell_Slots_Level_9 { get; set; }
+                        }
+                        
+                
 
+                        public class Feature
+                        {
+                            public string Index { get; set; }
+                            public string Name { get; set; }
+                            public string Url { get; set; }
+                        }
+
+                        public class Class
+                        {
+                            public string Index { get; set; }
+                            public string Name { get; set; }
+                            public string Url { get; set; }
+                        }
+
+                        public class BardLeveresou
+                        {
+                            public int? Level { get; set; }
+                            public int? Ability_Score_Bonuses { get; set; }
+                            public int? Prof_Bonus { get; set; }
+                            public List<Feature> Features { get; set; }
+                            public Spellcasting Spellcasting { get; set; }
+                            public ClassSpecific Class_Specific { get; set; }
+                            public string Index { get; set; }
+                            public Class Class { get; set; }
+                            public string Url { get; set; }
+                        }
+
+
+                        public class ClassSpecific
+                        {
+                            public int? Bardic_Inspiration_Die { get; set; }
+                            public int? Song_Of_Rest_Die { get; set; }
+                            public int? Magical_Secrets_Max_5 { get; set; }
+                            public int? Magical_Secrets_Max_7 { get; set; }
+                            public int? Magical_Secrets_Max_9 { get; set; }
+                        }
+                    }
+                }
+                
+
+            }
+        **/
     }
-
     
 }

59451b08405ff81c1ca5d3bd0ecd84cbc3fb19da
EpsteinRoss-FS <repstein@student.fullsail.edu>
2024-04-22 11:58:12 -0400

    Gets list of conditions and codition details.

---
+++ DndApi/DndApi/Controllers/RulesController.cs
@@ -0,0 +1,73 @@
+﻿using DndApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DndApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RulesController(HttpClient client) : ControllerBase
+    {
+        private HttpClient _client = client;
+        private string baseUrl = "https://www.dnd5eapi.co/api/";
+
+        [HttpGet]
+        [Route("/getAllConditions")]
+        public async Task<ActionResult<RulesModel.Conditions.ConditionsList>> GetAllClasses()
+        {
+            try
+            {
+                // URL from where you fetch the data
+
+
+                // Fetch data from the URL
+                var conditionsList = await _client.GetFromJsonAsync<RulesModel.Conditions.ConditionsList>($"{baseUrl}/conditions");
+
+                if (conditionsList == null)
+                {
+                    return NotFound();
+                }
+
+                return conditionsList;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("/getCondition/{id}")]
+        public async Task<object> GetConditionDetails(string id)
+        {
+            try
+            {
+                // URL from where you fetch the data
+
+
+                // Fetch data from the URL
+                var response = await _client.GetAsync($"{baseUrl}/conditions/{id}");
+
+                if (response == null)
+                {
+                    return NotFound();
+                }
+
+                var content = await response.Content.ReadAsStringAsync();
+    
+                var condition = JsonConvert.DeserializeObject<RulesModel.Conditions.Condition>(content);
+
+
+                
+                return Ok(condition);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it appropriately
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+    }
+}

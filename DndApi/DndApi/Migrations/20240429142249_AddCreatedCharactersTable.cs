a2459b1be2fec05ea1effd6a02fdf4ed46f869fd
EpsteinRoss-FS <repstein@student.fullsail.edu>
2024-04-29 10:24:05 -0400

    Added migration for CreatedCharacters

---
+++ DndApi/DndApi/Migrations/20240429142249_AddCreatedCharactersTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DndApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedCharactersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "created_characters",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    character_name = table.Column<string>(type: "text", nullable: true),
+                    character_level = table.Column<int>(type: "integer", nullable: true),
+                    character_class = table.Column<string>(type: "text", nullable: true),
+                    character_hitpoints = table.Column<int>(type: "integer", nullable: true),
+                    isDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_created_characters", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "created_characters");
+        }
+    }
+}
